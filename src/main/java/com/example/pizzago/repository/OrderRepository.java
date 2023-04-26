40904d724d7e68dabfcbfd8f655c61dcaddd966d
Colonel-Aziret <Aziret5265@gmail.com>
2023-04-26 11:02:53 +0600

    create repositories

---
+++ src/main/java/com/example/pizzago/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.example.pizzago.repository;
+
+import com.example.pizzago.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
---
+++ src/main/java/com/example/pizzago/repository/PizzaRepository.java
@@ -0,0 +1,9 @@
+package com.example.pizzago.repository;
+
+import com.example.pizzago.model.Pizza;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PizzaRepository extends JpaRepository<Pizza, Long> {
+}
