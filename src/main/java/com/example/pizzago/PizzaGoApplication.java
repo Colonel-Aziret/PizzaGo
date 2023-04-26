69d0fefb29fdf54efa65193236192dd56db4796c
Colonel-Aziret <Aziret5265@gmail.com>
2023-04-26 10:56:42 +0600

    initial commit

---
+++ src/main/java/com/example/pizzago/PizzaGoApplication.java
@@ -0,0 +1,13 @@
+package com.example.pizzago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzaGoApplication.class, args);
+    }
+
+}
