9146bb75595d1f6b14ae12182fb9895ee7a6014c
Colonel-Aziret <Aziret5265@gmail.com>
2023-05-03 01:04:00 +0600

    add enums

---
+++ src/main/java/com/example/pizzago/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.example.pizzago.enums;
+
+public enum OrderStatus {
+    PENDING,
+    PROCESSED,
+    SHIPPED,
+    DELIVERED,
+    CANCELLED
+}
---
+++ src/main/java/com/example/pizzago/enums/Role.java
@@ -0,0 +1,7 @@
+package com.example.pizzago.enums;
+
+public enum Role {
+    ADMIN,
+    CUSTOMER
+}
+
